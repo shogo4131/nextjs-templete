5d15461649bc3d58c3a0dd7ffebd6c4471ff1506
shogo4131 <shougo08697118@gmail.com>
2022-05-25 17:40:56 +0900

    first commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  swcMinify: true,
+  compiler: {
+    removeConsole: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
