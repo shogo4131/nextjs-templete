05bc7dfd18ee6dbc5d96ad6741a8c42aa6b3b60d
shogo4131 <shougo08697118@gmail.com>
2023-04-17 23:11:12 +0900

    fix: migration

--- src/components/Button/index.stories.tsx
+++ src/components/Button/index.stories.tsx
@@ -1,12 +1,12 @@
-import { ComponentMeta, ComponentStoryObj } from '@storybook/react';
+import { Meta, StoryObj } from '@storybook/react';
 
 import { Button } from './index';
 
 export default {
   component: Button,
-} as ComponentMeta<typeof Button>;
+} as Meta<typeof Button>;
 
-export const Default: ComponentStoryObj<typeof Button> = {
+export const Default: StoryObj<typeof Button> = {
   args: {
     children: 'テスト',
   },
